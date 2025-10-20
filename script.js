74d687a95857b64b77a9b400997b33db6ea03a09
Raja-Ali11 <m.alirajpoot2003@gmail.com>
2025-10-20 14:51:27 +0500

    css and java script added by Ali

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("showMsgBtn").addEventListener("click", function() {
+    const msg = document.getElementById("message");
+    msg.textContent = "Hello from Waqas and Ali! 🎉 This project is successfully connected with GitHub.";
+    msg.style.color = "#0077b6";
+    msg.style.fontWeight = "bold";
+});
