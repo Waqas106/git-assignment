4fdca0a403d7b4041cffd1efa2d88cb884ca9715
Raja-Ali11 <m.alirajpoot2003@gmail.com>
2025-10-20 15:20:22 +0500

    updated script by Muhammad Ali

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ document.getElementById("showMsgBtn").addEventListener("click", function() {
     msg.textContent = "Hello from Waqas and Ali! 🎉 This project is successfully connected with GitHub.";
     msg.style.color = "#0077b6";
     msg.style.fontWeight = "bold";
+     msg.style.color = "#2c0f5bff";
+    
 });
